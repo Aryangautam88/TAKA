18eba255f11c4e6d53ed27242dbf174e5c25d0b1
Kritikas8349 <kritikasingh8349@gmail.com>
2026-03-18 10:46:16 +0530

    first commit

--- ClIENT/src/App.jsx
+++ ClIENT/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
-// import About from "./pages/About";
+import About from "./pages/About";
 // import Services from "./pages/Services";
 // import Contact from "./pages/Contact";
 // import Career from "./pages/Career";
@@ -20,7 +20,7 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/about" element={<About />} />
           {/* <Route path="/services" element={<Services />} /> */}
           {/* <Route path="/contact" element={<Contact />} /> */}
           {/* <Route path="/career" element={<Career />} /> */}
