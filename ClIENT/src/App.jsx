73cc9bf210c3f125af7c9e3804bba2b6a0f04d28
Aryangautam88 <gautamaryan285@gmail.com>
2026-03-19 14:31:11 +0530

    push

--- ClIENT/src/App.jsx
+++ ClIENT/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Services from "./pages/Services";
+import Services from "./pages/Services";
 // import Contact from "./pages/Contact";
 // import Career from "./pages/Career";
 // import SignupInfluencer from "./pages/SignupInfluencer";
@@ -21,7 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/services" element={<Services />} /> */}
+          <Route path="/services" element={<Services />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
           {/* <Route path="/career" element={<Career />} /> */}
           {/* <Route path="/signup-influencer" element={<SignupInfluencer />} /> */}
